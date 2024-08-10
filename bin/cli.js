5e697a90b4b77d90e33d5da267a669e7f23509fa
miladbn <milad.b.n.77@gmail.com>
2024-08-10 11:36:23 +0330

    remove remover

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 import { program } from "commander";
 import { scan } from "../src/scanner.js";
 import { report } from "../src/reporter.js";
-import { remove } from "../src/remover.js";
+// import { remove } from "../src/remover.js";
 program
   .command("scan <path>")
   .description("Scan project for unused CSS")
@@ -13,6 +13,6 @@ program
   .description("Generate a report of unused CSS")
   .action(report);
 
-program.command("remove").description("Remove unused CSS").action(remove);
+// program.command("remove").description("Remove unused CSS").action(remove);
 
 program.parse(process.argv);
